f91f16f0ccdbc70cda27bd10eaec8ab4632bf27e
Adrian Miller <adrian.m.miller@gmail.com>
2023-12-23 05:26:40 +1100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+git init --initial-branch=main
+sleep
+git remote add origin https://gitlab.com/adrian.m.miller/dnscrypt-proxy-maintenance-script-android-sh.git
+sleep
+git add .
+sleep
+git commit -m "Initial commit"
+sleep
+git push --set-upstream origin main
