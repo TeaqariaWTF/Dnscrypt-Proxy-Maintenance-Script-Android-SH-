7f35b73bcfc62571e934cb00c1a2b04a990ba1a2
Adrian Miller <adrian.m.miller@gmail.com>
2024-01-01 04:13:22 +1100

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-git init --initial-branch=main
-sleep
-git remote add origin https://gitlab.com/adrian.m.miller/dnscrypt-proxy-maintenance-script-android-sh.git
-sleep
-git add .
-sleep
-git commit -m "Initial commit"
-sleep
-git push --set-upstream origin main
